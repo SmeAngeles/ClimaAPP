79a5db6b586e12bd30aa00054bd788f146dbc23f
Esmeralda Angeles <esmeralda.angeles@bancoazteca.com>
2021-06-10 22:07:03 -0500

    Initial Commit

---
+++ ClimaAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClimaAPP
+//
+//  Created by Esmeralda Angeles Mendoza on 10/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
